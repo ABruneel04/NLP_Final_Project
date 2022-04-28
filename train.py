29ec87f293407b430c02f1a899bf37f439c1513e
Andrew Bruneel <54682743+ABruneel04@users.noreply.github.com>
2022-04-27 20:35:44 -0700

    Update train.py

--- train.py
+++ train.py
@@ -117,9 +117,8 @@ def train():
             # Making a list to store captions used for bleu scores
             bleu_captions = list()
             # Building list of captions split on spaces
-            for caption in captions[:-1]:
-                bleu_captions.append(caption.split((1, 1))
-                print(caption.split(1,1))
+            # for caption in captions[:-1]:
+                # bleu_captions.append(caption.split((1, 1))
             # Calculating bleu score by each sentence
             # line_item_bleu_score = sentence_bleu(bleu_captions, outputs.shape[2])
             # all_bleu_scores.append(line_item_bleu_score)
