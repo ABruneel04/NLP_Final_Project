2c04353fea868d3b472e46d7b4b52066530e5610
Andrew Bruneel <54682743+ABruneel04@users.noreply.github.com>
2022-04-27 20:27:42 -0700

    Update train.py

--- train.py
+++ train.py
@@ -123,7 +123,7 @@ def train():
             for caption in captions[:-1]:
                 bleu_captions.append(caption.split((1, 1))
             # Calculating bleu score by each sentence
-            all_bleu_scores.append(sentence_bleu(bleu_captions, outputs))
+            all_bleu_scores.append(sentence_bleu(bleu_captions, outputs.shape[2]))
             # Calculating BLEU score for the epoch
             bleu_sum = sum(all_bleu_scores)
             bleu_len = len(all_bleu_scores)
